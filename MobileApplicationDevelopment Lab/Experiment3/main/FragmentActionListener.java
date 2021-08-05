4ad72e1712b2d0042d5ac0bb1215fe156fc25d8f
KujalaDivya25 <53556643+KujalaDivya25@users.noreply.github.com>
2021-08-05 18:33:49 +0530

    Create FragmentActionListener.java

---
+++ MobileApplicationDevelopment Lab/Experiment3/main/FragmentActionListener.java
@@ -0,0 +1,12 @@
+package com.example.madexp3;
+
+/**
+ * Created by anildeshpande on 12/26/17.
+ */
+
+public interface FragmentActionListener {
+
+    String KEY_SELECTED_CANDIDATE="KEY_SELECTED_CANDIDATE";
+
+    void onCandidateSelected(String candidate);
+}
